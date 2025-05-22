6472a06173142c0b05539c44d0d6b298e4b700c4
Barbara Ruiz <114244397+BarbaraR06@users.noreply.github.com>
2025-05-22 15:49:51 -0300

    arreglando URI

--- my-portfolio2/app/api/auth/callback/spotify/route.ts
+++ my-portfolio2/app/api/auth/callback/spotify/route.ts
@@ -3,12 +3,11 @@ import spotifyApi from '@/utils/spotify';
 
 export const dynamic = 'force-dynamic';
 
-// Determinar la URL base según el ambiente
 const BASE_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://portfolio2-0.vercel.app'
-  : 'https://localhost:3000';
+  ? 'https://portfolio2-0-ochre-chi.vercel.app'
+  : 'http://localhost:3000';
 
-// Validar que la URL de redirección sea HTTPS en producción
+
 const validateRedirectUri = () => {
   const redirectUri = process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URI;
   if (process.env.NODE_ENV === 'production' && !redirectUri?.startsWith('https://')) {
@@ -19,7 +18,6 @@ const validateRedirectUri = () => {
 
 export async function GET(request: Request) {
   try {
-    // Validar la URL de redirección
     validateRedirectUri();
 
     const { searchParams } = new URL(request.url);
@@ -36,22 +34,19 @@ export async function GET(request: Request) {
 
     const data = await spotifyApi.authorizationCodeGrant(code);
     
-    // Create a response with the tokens
     const response = NextResponse.redirect(BASE_URL);
     
-    // Set cookies with the tokens
     response.cookies.set('spotify_access_token', data.body.access_token, {
       maxAge: data.body.expires_in,
       httpOnly: true,
-      secure: true, // Siempre usar secure cookies
+      secure: true, 
       path: '/',
       sameSite: 'lax'
     });
     
     response.cookies.set('spotify_refresh_token', data.body.refresh_token, {
-      maxAge: 30 * 24 * 60 * 60, // 30 days
-      httpOnly: true,
-      secure: true, // Siempre usar secure cookies
+      maxAge: 30 * 24 * 60 * 60, 
+      secure: true, 
       path: '/',
       sameSite: 'lax'
     });
