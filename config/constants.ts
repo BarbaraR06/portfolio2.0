e7943c38eb15304f7d4bc24a2c8e35a70d77ffbb
Barbara Ruiz <114244397+BarbaraR06@users.noreply.github.com>
2025-05-28 16:42:46 -0300

    api spotify

--- config/constants.ts
+++
@@ -1,8 +0,0 @@
-// Contact Information
-export const CONTACT_EMAIL = "barbararuiz063@gmail.com";
-
-// Social Media Links
-export const GITHUB_URL = "https://github.com/BarbaraR06";
-
-// Resume
-export const CV_FILE = "/Barbara-Ruiz-CV.pdf";
