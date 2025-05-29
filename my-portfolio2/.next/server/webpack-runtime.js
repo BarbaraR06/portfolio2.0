e2fd7c19620b7f55727a0f48992767a0b0b361cb
Barbara Ruiz <114244397+BarbaraR06@users.noreply.github.com>
2025-05-29 08:41:30 -0300

    delete cloudinary

--- my-portfolio2/.next/server/webpack-runtime.js
+++ my-portfolio2/.next/server/webpack-runtime.js
@@ -30,7 +30,7 @@
 /******/ 		// Execute the module function
 /******/ 		var threw = true;
 /******/ 		try {
-/******/ 			__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 			__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
 /******/ 			threw = false;
 /******/ 		} finally {
 /******/ 			if(threw) delete __webpack_module_cache__[moduleId];
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("3b8e22c5478e5c9f")
+/******/ 		__webpack_require__.h = () => ("d6c91a0e12ba943f")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- my-portfolio2/app/providers.tsx
+++ my-portfolio2/app/providers.tsx
@@ -5,7 +5,6 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import * as React from "react";
 import { HeroUIProvider } from "@heroui/system";
 import { useRouter } from "next/navigation";
-import { CloudinaryContext } from 'cloudinary-react';
 
 export interface ProvidersProps {
   children: React.ReactNode;
@@ -16,10 +15,8 @@ export function Providers({ children, themeProps }: ProvidersProps) {
   const router = useRouter();
 
   return (
-    <CloudinaryContext cloudName="dgft27lky">
       <HeroUIProvider navigate={router.push}>
         <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
       </HeroUIProvider>
-    </CloudinaryContext>
   );
 }
