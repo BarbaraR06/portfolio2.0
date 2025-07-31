2668da43a4368e2540b7b26043e6e7bd790b7e8b
Barbara Ruiz <114244397+BarbaraR06@users.noreply.github.com>
2025-07-31 17:15:36 -0300

    fix

--- my-portfolio2/.next/server/webpack-runtime.js
+++ my-portfolio2/.next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("0764b2c9631a8b62")
+/******/ 		__webpack_require__.h = () => ("c955f73d27df3af1")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- my-portfolio2/app/page.tsx
+++ my-portfolio2/app/page.tsx
@@ -118,7 +118,7 @@ export default function Home() {
 
       {openTab && !minimizedTabs.includes(openTab) && (
         <Modal
-          title={t(openTab.toLowerCase())}
+          title={t(openTab)}
           onClose={() => handleCloseTab(openTab)}
           onMinimize={() => handleMinimizeTab(openTab)}
         >
